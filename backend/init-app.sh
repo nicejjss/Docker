32862439efb78d93866aed8ac49176a8d349e626
loc.daoduc <loc.daoduc@vti.com.vn>
2023-10-04 09:11:01 +0700

    update frontend

--- backend/init-app.sh
+++ backend/init-app.sh
@@ -2,7 +2,7 @@
 php artisan key:generate
 php artisan cache:clear
 
-/usr/local/bin/wait-for-it.sh mysql:3306 -t 300
+/usr/local/bin/wait-for-it.sh mysql:3306 -t 1000
 php artisan migrate
 php artisan passport:key
 
